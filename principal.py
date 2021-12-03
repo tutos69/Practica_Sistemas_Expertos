652613f241f1f741488b2387e3b488324de2c693
tutos69 <edwinz-2@outlook.com>
2021-12-03 18:50:18 -0500

    Prueba

--- principal.py
+++ principal.py
@@ -7,4 +7,8 @@ from flask import Flask, app
 app = Flask(__name__)
 @app.route('/')
 def indice():
-    return '<h1>Hola mundo</h1>'
+    return '<h1>Hola mundo como estas </h1>'
+
+if __name__=='__main__':
+    app.run(debug=True)
+
