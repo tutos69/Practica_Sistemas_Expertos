dce39847d2caa7e4f1086b262ace1870ae8de85b
tutos69 <edwinz-2@outlook.com>
2021-12-03 20:46:18 -0500

    restructuracion

--- principal.py
+++ principal.py
@@ -9,9 +9,39 @@ app = Flask(__name__)
 def indice():
     return render_template('Index.html')
 
-@app.route('/Tutores.html')
-def indice():
+@app.route('/tutorias')
+def tutorias():
+    return render_template('tutorias.html')
+
+@app.route('/tutores')
+def tutores():
     return render_template('Tutores.html')
+
+@app.route('/horarios')
+def horarios():
+    return render_template('Horarios.html')
+
+
+@app.route('/beneficios')
+def beneficios():
+    return render_template('Beneficios.html')
+
+@app.route('/reglamento')
+def reglamento():
+    return render_template('Reglamento.html')
+
+@app.route('/tepes')
+def tepes():
+    return render_template('Tepes.html')
+
+@app.route('/conocemos')
+def conocemos():
+    return render_template('AcercaDe.html')
+
+
+@app.route('/contacto')
+def contacto():
+    return render_template('formulario.html')    
 
 
 app.run(debug=True, port=8000)
